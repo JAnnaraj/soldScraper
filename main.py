0979cf2f8dd16ab5dc26df34130ce79e2484151f
Jeffrey Annaraj <JAnnaraj@bluevalleyk12.net>
2020-11-21 07:29:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import lxml
+from bs4 import BeautifulSoup
+import requests
+
+
+keyword = input("Keyword? ")
+
+source = requests.get(f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={keyword}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_BIN=1&_samilow=&_samihi=&_sadis=15&_stpos=66085&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50&LH_Complete=1&_fosrp=1').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+results = soup.find_all('h3',class_ = 's-item__title s-item__title--has-tags')
+
+for result in results:
+  print(result.prettify())
