8f5c248099328d83d7562ff6b17efa89e8f34840
Jeffrey Annaraj <JAnnaraj@bluevalleyk12.net>
2020-11-23 03:41:31 +0000

    added prices scraper

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ keyword = input("Keyword? ")
 source = requests.get(f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={keyword}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_BIN=1&_samilow=&_samihi=&_sadis=15&_stpos=66085&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50&LH_Complete=1&_fosrp=1').text
 soup = BeautifulSoup(source, 'lxml')
 #result(s)
-results = soup.find_all('span',class_ = 'bold bidsold')
-
+results_price = soup.find_all('span',class_ = 'bold bidsold')
+results_title = soup.find_all('a', class_ = "vip")
 #parser(s)
+prices = []
 
-i = 1
-while i <= 5:
-    print(results[i])
-    i += 1
+for price in results_price:
+  if price == "Trending at":
+    pass
+  if "to" in price:
+    pass
+  else:
+    prices.append(price.text.strip('Trendingatto<span>\n\t'))
 
-# for result in results:
-#   if x < 3:
-#     print(result.prettify())
-#     x += 1
-
-# i = 1
-# while i <= 100:
-#     print("Today is Sunday")
-#     i += 1
+for item in prices:
+  print(item)
